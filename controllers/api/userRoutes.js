47e118829adc5a1614e0271954f9aa354f0ab161
wingram1 <waingram96@gmail.com>
2023-03-08 11:34:43 -0500

    user routes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,11 +1,28 @@
 const router = require("express").Router();
+const { User } = require("../../models");
+
 router.get("/", async (req, res) => {
   try {
-    // get posts
-    console.log("Hello userRoutes!");
-    res.send(200);
-  } catch {
-    res.send(err);
+    const userData = await User.findAll({});
+
+    res.json(userData);
+  } catch (err) {
+    console.log("Error fetching users");
+    res.sendStatus(500).json(err);
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const newUser = await User.create({
+      username: req.body.username,
+      password: req.body.password,
+    });
+
+    res.json(newUser);
+  } catch (err) {
+    console.log("Error");
+    res.status(500).json(err);
   }
 });
 module.exports = router;
